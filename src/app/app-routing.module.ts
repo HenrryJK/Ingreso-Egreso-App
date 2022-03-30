06a55427e46dc9104d36c567ceae1592b13a66c0
HenrryJK <henrryorbezo@upeu.edu.pe>
2022-03-30 12:20:55 -0500

    #3 Creando Rutas Hijas de la aplicacion

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 import { DashboardComponent } from "./auth/dashboard/dashboard.component";
+import { dashboardRoutes } from "./auth/dashboard/dashboard.routes";
 import { LoginComponent } from "./auth/login/login.component";
 import { RegisterComponent } from "./auth/register/register.component";
 
@@ -8,7 +9,9 @@ import { RegisterComponent } from "./auth/register/register.component";
 const routes: Routes = [
   {path: 'login' , component:LoginComponent},
   {path: 'register' , component:RegisterComponent},
-  {path:'',component:DashboardComponent},
+  // el children sirve para llamar a rutas hijas , q recibe una coleccion de rutas
+  {path:'',component:DashboardComponent,
+   children:dashboardRoutes},
   // los "**" es para indicar q cualquier otra direccion que haga y no pertenezca a ningun lado lo redireccione al punto principal q seria el dashboard
   {path:'**' , redirectTo:''}
 
